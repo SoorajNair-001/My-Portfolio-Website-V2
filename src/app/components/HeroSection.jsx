d4152fb42700d64e512593eb7e3980d0e4e223bf
Sooraj Sunil Nair <89830630+SoorajNair-001@users.noreply.github.com>
2024-10-08 03:56:09 -0230

    update section

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -50,17 +50,6 @@ const HeroSection = () => {
                     <Link href="https://www.linkedin.com/in/sooraj-nair01/" target={"_blank"}>
                         <Image src={LinkedinIcon} alt="Linkedin Icon" className="py-3"/>
                     </Link>
-                    <div>
-                    <Link
-                        href="/Resume.pdf"
-                        target={"_blank"}
-                        className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
-                    >
-                    <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                        Download CV
-                    </span>
-                    </Link>
-                    </div>
                 </div>
             </div>
         </motion.div>
